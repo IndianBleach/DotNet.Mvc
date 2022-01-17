d67129ad5a3a9be1e9d80f9fdeabcba7d7a33b5c
Boris <AlabamaBleach@gmail.com>
2022-01-17 23:27:47 +0300

    fix bugs and update

---
+++ Mvc.WebUi/Components/IdeaMenuViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.WebUi.Components
+{
+    public class IdeaMenuViewComponent : ViewComponent
+    {
+        public IdeaMenuViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
